6ac8fe2e51face9a25c6ca8fc5f87a5968d7ba4f
Joshua <jssmiller94@gmail.com>
2021-05-04 09:07:30 +0100

    finished homework, 3/9 correct

---
+++ scope_hw.js
@@ -0,0 +1,224 @@
+
+
+// Episode 1
+
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// function will return Miss Scarlet as murderer because on line 13 the declareMurderer function calls the key value pair scenario.murderer which is within scope.
+// ➜  06_hw_scope_whodunnit node scope_hw.js                                                               
+//  The murderer is Miss Scarlet.
+
+
+// Episode 2
+
+
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Mrs. Peacock will be returned as the murderer #INCORRECT
+// error message: Assignment to constant variable. Const variables cannot be reassigned.
+
+
+// Episode 3
+
+
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+// firstVerdict will return Mrs. Peacock because that assignment is within the called function.
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+// secondVerdict will return Mrs. Peacock because murderer as been reassigned on line 55 #INCORRECT
+// returned professor plum because the assignment that happens on line 55 is out of scope and separate
+
+
+// Episode 4
+
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// should return Miss Scarlet, Professor Plum, and Colonel Mustard
+// console.log(`Suspect three is ${suspectThree}.`);
+// suspect three this time will be Mrs. Peacock
+// ➜  06_hw_scope_whodunnit node scope_hw.js
+// The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.
+// Suspect three is Mrs. Peacock.
+
+
+// Episode 5
+
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+// will return error as the key value pairs in const scenario cannot be changed #INCORRECT
+// ➜  06_hw_scope_whodunnit node scope_hw.js
+// The weapon is the Revolver.
+// why was this possible?
+
+
+// Episode 6
+
+
+// let murderer = 'Colonel Mustard';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// will return Mrs White
+
+
+// Episode 7
+
+
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// will return Colonel Mustard #INCORRECT
+
+
+// Episode 8
+
+
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
+
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
+
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
+
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
+
+//     unexpectedOutcome('Colonel Mustard');
+//   }
+
+//   plotTwist('Dining Room');
+// }
+
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
+
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
+// will return lead pipe as weapon #INCORRECT
+
+
+// Episode 9
+
+
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+// will return Prof PLum
